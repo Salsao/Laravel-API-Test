5a9f153202d9edb56f949a958ced7c186a5c4a69
Eduardo Sára Pereira <eduardosarapereira@gmail.com>
2018-05-19 18:09:39 -0300

    Validation for teams

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -5,10 +5,32 @@ namespace App\Http\Controllers;
 use App\Team;
 use App\Match;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use finfo;
 
 class TeamController extends Controller
 {
+    private function errorMessage($message) 
+    {
+        return response()->json(array(
+                'status' => 'error',
+                'message' => $message
+            ), 500);
+    }
+
+    public function checkFields($request) 
+    {
+        return Validator::make($request->all(), [
+                'name' => 'required',
+                'image' => 'required|image|max:16000'
+            ],[
+                'name.required' => 'Team name is required.',
+                'image.required' => 'Image is required.',
+                'image.image' => 'Image must be in an image extension.',
+                'image.max' => 'Image exceeds maximum size(16 megabytes).'
+        ]);
+    }
+
     public function index()
     {
         $teams = Team::all();
@@ -43,6 +65,12 @@ class TeamController extends Controller
 
     public function store(Request $request)
     {
+        $validator = $this->checkFields($request);
+
+        if ($validator->fails()) {
+            return $this->errorMessage($validator->messages());
+        }
+
         $file = $request->file('image');
         $contents = $file->openFile()->fread($file->getSize());
 
@@ -59,6 +87,12 @@ class TeamController extends Controller
 
     public function update(Request $request, Team $team)
     {
+        $validator = $this->checkFields($request);
+
+        if ($validator->fails()) {
+            return $this->errorMessage($validator->messages());
+        }
+
         $file = $request->file('image');
         $contents = $file->openFile()->fread($file->getSize());
 
