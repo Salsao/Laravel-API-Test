bd5665c2d9c7d0d0f3c9316e9d85a2b0a7b3430f
Eduardo Sára Pereira <eduardosarapereira@gmail.com>
2018-05-19 17:46:28 -0300

    Validation for matches

--- app/Http/Controllers/MatchController.php
+++ app/Http/Controllers/MatchController.php
@@ -4,9 +4,33 @@ namespace App\Http\Controllers;
 
 use App\Match;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class MatchController extends Controller
 {
+    private function errorMessage($message) {
+        return response()->json(array(
+                'status' => 'error',
+                'message' => $message
+            ), 500);
+    }
+
+    public function checkFields($request) 
+    {
+        return Validator::make($request->all(), [
+                'team_a_id' => 'required|exists:teams,id|different:team_b_id',
+                'team_b_id' => 'required|exists:teams,id',
+                'datetime' => 'required|date_format:Y-m-d H:i:s'
+            ],[
+                'team_a_id.required' => 'Team A is required.',
+                'team_a_id.exists' => 'Team A doesn\'t exist.',
+                'team_a_id.different' => 'The teams must be different.',
+                'team_b_id.required' => 'Team B is required.',
+                'team_b_id.exists' => 'Team B doesn\'t exist.',
+                'datetime.date_format' => 'The date does not match the format YYYY-mm-dd HH:MM:SS.'
+        ]);
+    }
+
     public function index()
     {
         return Match::all();
@@ -24,6 +48,12 @@ class MatchController extends Controller
 
     public function store(Request $request)
     {
+        $validator = $this->checkFields($request);
+
+        if ($validator->fails()) {
+            return $this->errorMessage($validator->messages());
+        }
+
         $match = Match::create($request->all());
 
         return response()->json($match, 201);
@@ -31,6 +61,12 @@ class MatchController extends Controller
 
     public function update(Request $request, Match $match)
     {
+        $validator = $this->checkFields($request);
+
+        if ($validator->fails()) {
+            return $this->errorMessage($validator->messages());
+        }
+
         $match->update($request->all());
 
         return response()->json($match, 200);
